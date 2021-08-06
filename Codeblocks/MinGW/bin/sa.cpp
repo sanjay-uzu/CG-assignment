d0a84924fbbb3024850fdc5cc3b06f14be17f1e8
sanjay-uzu <65504697+sanjay-uzu@users.noreply.github.com>
2021-08-06 15:58:47 +0530

    commit

--- Codeblocks/MinGW/bin/sa.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-int a[10];
-printf("%d",*a+1-*a+3);
-}
--- Codeblocks/MinGW/mingw-w64.bat
+++
@@ -1,5 +0,0 @@
-echo off
-call DevelFolder.bat
-set PATH=%MINGW_64%;%PATH%
-cd "C:\"
-"C:\Windows\system32\cmd.exe"
